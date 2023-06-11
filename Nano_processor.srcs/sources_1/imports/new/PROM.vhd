deceb1f9b7d4fb81439e7d6a9f46e752106c38c5
SaviNimz <howfeels189@gmail.com>
2023-06-11 22:53:08 +0530

    updated prom

--- Nano_processor.srcs/sources_1/imports/new/PROM.vhd
+++ Nano_processor.srcs/sources_1/imports/new/PROM.vhd
@@ -21,6 +21,20 @@ type rom_type is array (0 to 7) of std_logic_vector(11 downto 0);
     "110000000111",  --JZR R0, 7 ; If R0 = 0 jump to line 7  11 000 0000 111    c00
     "000000000000",
     "000000000000"
+    
+    -- second set of instructions
+    
+       --signal Program_Rom : rom_type := (
+       --stored instructions (indexing from 0 - 7)
+        --"101110000001",  --MOVI R7,1   10 111 000 0001     881
+        --"100100000010",  --MOVI R2,2   10 010 000 0010     901  
+        --"100110000011",  --MOVI R3,3   10 011 000 0011     0a0
+        --"001110100000",  --ADD R7,R2    00 111 010 0000     0a0
+        --"001110110000",  --ADD R7,R3    00 111 011 0000     0a0
+        --"110000000111",  --JZR R0, 7 ; If R0 = 0 jump to line 7  11 000 0000 111    c00
+        --"000000000000",
+        --"000000000000"
+    --);
 );
 
 begin
